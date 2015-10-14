8caca765fa9f5e2476e22ceea967f69bd42dc438
Pauline Adam <pauline.adam@mastertp>
2015-10-14 12:12:13 +0200

    pause mercredi

--- js/aLettre.js
+++ js/aLettre.js
@@ -1,7 +1,13 @@
 function displayLetter(letter){
   
+  //Display the corresponding letter in uppercase
   $("#result-majuscule").text(letter.toUpperCase());
+  
+  //Display the image of the corresponding letter in cursive
   $("#result-cursive").attr("src","../images/lettresCursives/"+ letter + "Cursif.png");
+  $("#result-cursive").attr("style","display:initial");
+  
+  //Play the pronunciation of the corresponding letter 
   $("#audioPlayer").attr("src", "../sons/" + letter + "Son.mp3");
   
 }
